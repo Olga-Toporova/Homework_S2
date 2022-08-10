3da97ac349e05ac7e20d8d5f4f46d4c2975de239
Olga Toporova <sova795@yandex.ru>
2022-08-10 13:51:06 +0500

    Выполнено задание 15

--- Ex_15/Program.cs
+++ Ex_15/Program.cs
@@ -7,20 +7,15 @@ Console.WriteLine("Является ли день выходным?");
 Console.WriteLine("1 - пн, 2 - вт, 3 - ср, 4 - чт, 5 - пт, 6 - сб, 7 - вс");
 Console.Write("Введите число от 1 до 7: ");
 int weekDay = Convert.ToInt32(Console.ReadLine());
-static void checkWeekDay(int weekDay)
+if(weekDay < 1 || weekDay > 7)
 {
-    if(weekDay < 1 || weekDay > 7)
-    {
-        Console.WriteLine("Введено некорректное значение");
-    }
-    if(weekDay == 6 || weekDay == 7)
-    {
-        Console.WriteLine("Да, это выходной день");
-    }
-    if(weekDay > 0 && weekDay < 6)
-    {
-        Console.WriteLine("Нет, это не выходной");
-    }
+    Console.WriteLine("Введено некорректное значение");
 }
-checkWeekDay(weekDay);
-
+if(weekDay == 6 || weekDay == 7)
+{
+    Console.WriteLine("Да, это выходной день");
+}
+if(weekDay > 0 && weekDay < 6)
+{
+    Console.WriteLine("Нет, это не выходной");
+}
