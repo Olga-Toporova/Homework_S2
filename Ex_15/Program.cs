665bb02afc22aeead05f198f5a76e385ffa71e31
Olga Toporova <sova795@yandex.ru>
2022-08-10 13:47:45 +0500

    Выполнено задание 15

---
+++ Ex_15/Program.cs
@@ -0,0 +1,26 @@
+﻿/* Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+6 -> да
+7 -> да
+1 -> нет */
+
+Console.WriteLine("Является ли день выходным?");
+Console.WriteLine("1 - пн, 2 - вт, 3 - ср, 4 - чт, 5 - пт, 6 - сб, 7 - вс");
+Console.Write("Введите число от 1 до 7: ");
+int weekDay = Convert.ToInt32(Console.ReadLine());
+static void checkWeekDay(int weekDay)
+{
+    if(weekDay < 1 || weekDay > 7)
+    {
+        Console.WriteLine("Введено некорректное значение");
+    }
+    if(weekDay == 6 || weekDay == 7)
+    {
+        Console.WriteLine("Да, это выходной день");
+    }
+    if(weekDay > 0 && weekDay < 6)
+    {
+        Console.WriteLine("Нет, это не выходной");
+    }
+}
+checkWeekDay(weekDay);
+
